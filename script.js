7dd0dbdf4127909162b29ba31fca2d01a6c30484
Raj tiwari <rt910352@gmail.com>
2025-08-02 14:59:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -91,6 +91,14 @@
       }
     });
   });
+const toggleBtn = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+toggleBtn.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
+
+
 
 
 
